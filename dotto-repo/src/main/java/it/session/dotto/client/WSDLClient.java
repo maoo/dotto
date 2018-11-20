d40d58982b3177e195ac98d3689859081edf3846
Maurizio Pillitu <maoo@apache.org>
2018-11-20 14:14:44 +0100

    removed wsdl logic

--- dotto-repo/src/main/java/it/session/dotto/client/WSDLClient.java
+++ dotto-repo/src/main/java/it/session/dotto/client/WSDLClient.java
@@ -6,13 +6,13 @@ import org.apache.commons.logging.Log;
 
 import org.apache.commons.logging.LogFactory;
 
-import it.session.dotto.generated.wsdl.HelloService;
-import it.session.dotto.generated.wsdl.HelloPortType;
+// import it.session.dotto.generated.wsdl.HelloService;
+// import it.session.dotto.generated.wsdl.HelloPortType;
 
 public class WSDLClient {
 
 	// @WebServiceRef(wsdlLocation="http://localhost:8080/helloservice/hello?wsdl")
-	HelloService service;
+	// HelloService service;
 
 	private static final String CALLBACK_URL = "http://spring.io/guides/gs-producing-web-service/GetCountryRequest";
 
@@ -20,15 +20,15 @@ public class WSDLClient {
 
     private String wsdlEndpoint = "http://localhost:8080/ws/Hello_Port";
 
-	public HelloPortType getHelloPort() {
-		HelloService request = new HelloService();
+	public void getHelloPort() {
+		// HelloService request = new HelloService();
 
 		logger.info("Requesting location for HelloPort");
 
-		HelloPortType response = service.getHelloPort();
+		// HelloPortType response = service.getHelloPort();
 
-        logger.info("Response: "+response.sayHello("test"));
-		return response;
+        // logger.info("Response: "+response.sayHello("test"));
+		// return response;
 	}
 
 }
