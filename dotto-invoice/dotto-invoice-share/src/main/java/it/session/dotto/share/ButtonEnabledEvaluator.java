bf2ddd68e2df59001d357e1dc85181a79dce2435
Maurizio Pillitu <maoo@apache.org>
2019-02-08 14:41:18 +0100

    added debug logging

--- dotto-invoice/dotto-invoice-share/src/main/java/it/session/dotto/share/ButtonEnabledEvaluator.java
+++ dotto-invoice/dotto-invoice-share/src/main/java/it/session/dotto/share/ButtonEnabledEvaluator.java
@@ -1,6 +1,8 @@
 package it.session.dotto.share;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 import org.alfresco.web.evaluator.BaseEvaluator;
@@ -16,6 +18,9 @@ public class ButtonEnabledEvaluator extends BaseEvaluator {
        InputStream in = classLoader.getResourceAsStream("alfresco-global.properties");       
        try {
             prop.load(in);
+            System.out.println("!!!! ALF GLOBAL CONTENT !!!!");
+            System.out.println(getStringFromInputStream(in));
+            System.out.println("!!!! END ALF GLOBAL CONTENT !!!!");
             this.enabled = new Boolean(prop.getProperty(propertyName));
             in.close();
         } catch (Exception e) {
@@ -26,4 +31,34 @@ public class ButtonEnabledEvaluator extends BaseEvaluator {
     public boolean evaluate(JSONObject jsonObject) {
         return this.enabled;
     }
+
+// convert InputStream to String
+private static String getStringFromInputStream(InputStream is) {
+
+    BufferedReader br = null;
+    StringBuilder sb = new StringBuilder();
+
+    String line;
+    try {
+
+        br = new BufferedReader(new InputStreamReader(is));
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        if (br != null) {
+            try {
+                br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    return sb.toString();
+
+}
 }
